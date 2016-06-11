055c2b84c984aa62afe83ede053f4395043a0efa
Chaitya Shah <Chaitya62@gmail.com>
2016-06-11 15:42:01 +0530

    Add Navigation Bar

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function Aboutme(){
 	$(".cover").toggleClass("is-clicked");
   $(".aboutMe").toggleClass("is-clicked");
+  $("#Home").toggleClass("HAboutme")
+}
+function Home(){
+	$(".cover").toggleClass("is-clicked");
+  $(".aboutMe").toggleClass("is-clicked");
 }
 function Resume(){
 	$(".cover").toggleClass("is-clicked");
   $(".Resume").toggleClass("is-clicked");
 }
 
+$("#AboutMenav").click(Aboutme);
 $("#AboutMe").click(Aboutme);
 $("#AboutMe1").click(Aboutme);
 $("#Resume").click(Resume);
