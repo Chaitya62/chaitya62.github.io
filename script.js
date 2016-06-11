5800bf846031e46bff92eb0b91089dea06710007
Chaitya Shah <Chaitya62@gmail.com>
2016-06-11 21:51:33 +0530

    Make Nav Bar Transparent

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function Aboutme(){
 	$(".cover").toggleClass("is-clicked");
   $(".aboutMe").toggleClass("is-clicked");
-  $("#Home").toggleClass("HAboutme")
-}
-function Home(){
-	$(".cover").toggleClass("is-clicked");
-  $(".aboutMe").toggleClass("is-clicked");
 }
 function Resume(){
 	$(".cover").toggleClass("is-clicked");
   $(".Resume").toggleClass("is-clicked");
 }
 
-$("#AboutMenav").click(Aboutme);
+if($(".cover").hasClass("is-clicked")){
+	$("#AboutMenav").click(Aboutme);
+}
 $("#AboutMe").click(Aboutme);
 $("#AboutMe1").click(Aboutme);
 $("#Resume").click(Resume);
 $("#Resume1").click(Resume);
+function Home(){
+	$(".cover").removeClass("is-clicked");
+  $(".aboutMe").removeClass("is-clicked");
+  $(".Resume").removeClass("is-clicked");
+}
+
+$('#Home').click(Home);
